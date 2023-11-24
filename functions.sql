a5d94e10bd958764f6165b1600bf7c685e2aab84
Matheus Martins da Rocha <124638744+MatheusMartinsR@users.noreply.github.com>
2023-11-24 15:13:12 -0300

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,38 +1,23 @@
-CREATE OR REPLACE FUNCTION validar_usuario(
-    p_id_usuario IN NUMBER,
-    p_email_usuario IN VARCHAR2,
-    p_senha_usuario IN VARCHAR2,
-    p_cnpj_usuario IN VARCHAR2,
-    p_cep_usuario IN VARCHAR2
-) RETURN BOOLEAN
-IS
+CREATE OR REPLACE FUNCTION validar_email(p_email IN VARCHAR2) RETURN BOOLEAN IS
+    v_pattern VARCHAR2(100) := '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}$';
 BEGIN
-    IF p_id_usuario IS NULL OR p_id_usuario <= 0 THEN
+    IF REGEXP_LIKE(p_email, v_pattern) THEN
+        RETURN TRUE;
+    ELSE
         RETURN FALSE;
     END IF;
-     IF p_email_usuario IS NULL OR LENGTH(p_email_usuario) > 255 THEN
-        RETURN FALSE;
-    END IF;
-RETURN TRUE;
-END;
+END validar_email;
 
 
-CREATE OR REPLACE FUNCTION validar_pedido(
-    p_id_pedido IN NUMBER,
-    p_id_usuario IN NUMBER,
-    p_nome_pedido IN VARCHAR2,
-    p_desc_pedido IN VARCHAR2,
-    p_data_pedido IN DATE,
-    p_preco_pedido IN VARCHAR2,
-    p_pedido_aceito IN CHAR
-) RETURN BOOLEAN
-IS
+CREATE OR REPLACE FUNCTION validar_cep(p_cep IN VARCHAR2) RETURN BOOLEAN IS
+    v_pattern VARCHAR2(100) := '^\d{5}-\d{3}$';
 BEGIN
-    IF p_id_pedido IS NULL OR p_id_pedido <= 0 THEN
+    IF REGEXP_LIKE(p_cep, v_pattern) THEN
+        RETURN TRUE;
+    ELSE
         RETURN FALSE;
     END IF;
-    IF p_id_usuario IS NULL OR p_id_usuario <= 0 THEN
-        RETURN FALSE;
-    END IF;
-    RETURN TRUE; 
-END;
+END validar_cep;
+
+
+
