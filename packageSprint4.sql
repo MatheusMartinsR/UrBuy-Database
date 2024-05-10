f4384b7f71e439b1a5f4276203977e08076e6908
Matheus Martins da Rocha <124638744+MatheusMartinsR@users.noreply.github.com>
2024-05-10 19:24:41 -0300

    feat: update Packages

--- packageSprint4.sql
+++ packageSprint4.sql
@@ -1,46 +1,29 @@
-CREATE OR REPLACE PACKAGE pacote_relatorios AS
+CREATE OR REPLACE PACKAGE meu_pacote AS
+    
+    PROCEDURE relatorio_pedidos_aceitos(
+        p_id_fornecedor IN tb_fornecedor.id_fornecedor%TYPE
+    );
+    
+    
+    PROCEDURE calcular_total_itens_comprados(
+        p_id_usuario IN tb_usuario.id_usuario%TYPE,
+        p_total_itens OUT NUMBER
+    );
+    
+    
     FUNCTION calcular_valor_total_compras(p_id_usuario IN NUMBER) RETURN NUMBER;
+    
+    
     FUNCTION calcular_preco_medio_produtos(p_id_fornecedor IN NUMBER) RETURN NUMBER;
-    PROCEDURE relatorio_pedidos_aceitos(p_id_fornecedor IN tb_fornecedor.id_fornecedor%TYPE);
-    PROCEDURE calcular_total_itens_comprados(p_id_usuario IN tb_usuario.id_usuario%TYPE, p_total_itens OUT NUMBER);
-END pacote_relatorios;
+END meu_pacote;
 
 
-CREATE OR REPLACE PACKAGE BODY pacote_relatorios AS
-    FUNCTION calcular_valor_total_compras(p_id_usuario IN NUMBER) RETURN NUMBER IS
-        v_total NUMBER := 0;
-    BEGIN
-        SELECT SUM(p.preco_produto)
-        INTO v_total
-        FROM tb_itemdecompra i
-        JOIN tb_produto p ON i.id_produto = p.id_produto
-        WHERE i.id_usuario = p_id_usuario;
-
-        RETURN v_total;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RETURN 0; 
-        WHEN OTHERS THEN
-            RETURN NULL; 
-    END calcular_valor_total_compras;
-
-    FUNCTION calcular_preco_medio_produtos(p_id_fornecedor IN NUMBER) RETURN NUMBER IS
-        v_preco_medio NUMBER := 0;
-    BEGIN
-        SELECT AVG(preco_produto)
-        INTO v_preco_medio
-        FROM tb_produto
-        WHERE id_fornecedor = p_id_fornecedor;
-
-        RETURN v_preco_medio;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RETURN 0; 
-        WHEN OTHERS THEN
-            RETURN NULL; 
-    END calcular_preco_medio_produtos;
-
-    PROCEDURE relatorio_pedidos_aceitos(p_id_fornecedor IN tb_fornecedor.id_fornecedor%TYPE) IS
+CREATE OR REPLACE PACKAGE BODY meu_pacote AS
+    
+    PROCEDURE relatorio_pedidos_aceitos(
+        p_id_fornecedor IN tb_fornecedor.id_fornecedor%TYPE
+    )
+    IS
         CURSOR cur_pedidos IS
             SELECT p.id_pedido, p.nome_pedido, p.desc_pedido, p.data_pedido, p.preco_pedido
             FROM tb_pedido p
@@ -70,8 +53,13 @@ CREATE OR REPLACE PACKAGE BODY pacote_relatorios AS
         WHEN OTHERS THEN
             DBMS_OUTPUT.PUT_LINE('Erro ao gerar relatório de pedidos aceitos: ' || SQLERRM);
     END relatorio_pedidos_aceitos;
-
-    PROCEDURE calcular_total_itens_comprados(p_id_usuario IN tb_usuario.id_usuario%TYPE, p_total_itens OUT NUMBER) IS
+    
+    
+    PROCEDURE calcular_total_itens_comprados(
+        p_id_usuario IN tb_usuario.id_usuario%TYPE,
+        p_total_itens OUT NUMBER
+    )
+    IS
     BEGIN
         SELECT COUNT(*)
         INTO p_total_itens
@@ -86,4 +74,42 @@ CREATE OR REPLACE PACKAGE BODY pacote_relatorios AS
         WHEN OTHERS THEN
             DBMS_OUTPUT.PUT_LINE('Erro ao calcular total de itens comprados: ' || SQLERRM);
     END calcular_total_itens_comprados;
-END pacote_relatorios;
+    
+  
+    FUNCTION calcular_valor_total_compras(p_id_usuario IN NUMBER) RETURN NUMBER
+    IS
+        v_total NUMBER := 0;
+    BEGIN
+        SELECT SUM(p.preco_produto)
+        INTO v_total
+        FROM tb_itemdecompra i
+        JOIN tb_produto p ON i.id_produto = p.id_produto
+        WHERE i.id_usuario = p_id_usuario;
+
+        RETURN v_total;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN 0; 
+        WHEN OTHERS THEN
+            RETURN NULL; 
+    END calcular_valor_total_compras;
+    
+    
+    FUNCTION calcular_preco_medio_produtos(p_id_fornecedor IN NUMBER) RETURN NUMBER
+    IS
+        v_preco_medio NUMBER := 0;
+    BEGIN
+        SELECT AVG(preco_produto)
+        INTO v_preco_medio
+        FROM tb_produto
+        WHERE id_fornecedor = p_id_fornecedor;
+
+        RETURN v_preco_medio;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RETURN 0; 
+        WHEN OTHERS THEN
+            RETURN NULL; 
+    END calcular_preco_medio_produtos;
+END meu_pacote;
+
